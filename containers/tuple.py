e6cd9c93dc44b5cf858b172c18936a6389d59236
rakesh <rakesh.ss@target.com>
2021-01-27 07:46:07 +0530

    tuple as part of dictionary key

--- containers/tuple.py
+++ containers/tuple.py
@@ -1,2 +1,6 @@
 t = (5, 6)        # Create a tuple
 print(type(t))
+
+d = {(x, x + 1): x for x in range(10)}  # Create a dictionary with tuple keys
+print(d[t])       # Prints "5"
+print(d[(1, 2)])  # Prints "1"
