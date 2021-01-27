6580ce2128dc013ee88c9f091a536442b4ccf6ac
rakesh <rakesh.ss@target.com>
2021-01-27 08:04:19 +0530

    simple class defination

---
+++ classes.py
@@ -0,0 +1,17 @@
+class Greeter(object):
+
+    #constructor defination
+
+    def __init__(self,name):
+        self.name = name # Creates an instance variable
+    
+    #instance method
+    def greet(self, loud=False):
+        if loud:
+            print('HELLO, %s!' % self.name.upper())
+        else:
+            print('Hello, %s' % self.name)
+
+g = Greeter('Fred')  # Construct an instance of the Greeter class
+g.greet()            # Call an instance method; prints "Hello, Fred"
+g.greet(loud=True)   # Call an instance method; prints "HELLO, FRED!"
