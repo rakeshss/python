c9b8de8de1320f57a1a4773dd8f825db39cebc6c
rakesh <rakesh.ss@target.com>
2021-01-27 06:07:34 +0530

    slice operations

--- containers/list.py
+++ containers/list.py
@@ -8,3 +8,20 @@ xs.append('bar')  # Add a new element to the end of the list
 print(xs)         # Prints "[3, 1, 'foo', 'bar']"
 x = xs.pop()      # Remove and return the last element of the list
 print(x, xs)      # Prints "bar [3, 1, 'foo']"
+
+
+# slicing: you can access more than one element at a time!! can create sublist et al
+
+nums = list(range(5))
+print(nums)  # prints:- [0, 1, 2, 3, 4]
+print(nums[2:4]) # prints:-  gets the slice [2, 3] , here index 4 is excluded
+print(nums[2:])  # prints:-  gets the slice [2, 3, 4] , here index 2 is included till the end
+
+# few other operations on slices..
+print(nums[:2])           # Get a slice from the start to index 2 (exclusive); prints "[0, 1]"
+print(nums[:])            # Get a slice of the whole list; prints "[0, 1, 2, 3, 4]"
+print(nums[:-1])          # Slice indices can be negative; prints "[0, 1, 2, 3]"
+nums[2:4] = [8, 9]        # Assign a new sublist to a slice
+print(nums)               # Prints "[0, 1, 8, 9, 4]"
+
+
