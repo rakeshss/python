90c6e1fc8722d2bc03f904b0bdda837a960fdca3
rakesh <rakesh.ss@target.com>
2021-01-30 14:23:37 +0530

    numpy sample example

---
+++ numpy/sample.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+# rank is 1
+a = np.array([1,2,3])
+
+print(a)
+print(a[0],a[1], a[2])
+print(type(a))
+print(a.shape)
+
+# rank is 2
+b = np.array([[1,2,3],[4,5,6]])
+print(b.shape)
+
